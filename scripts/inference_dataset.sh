20b1f0de87b1c8c46b84786701993c6898814747
Alterex1 <castilloerick238@gmail.com>
2025-12-05 22:09:48 -0500

    initial upload

---
+++ scripts/inference_dataset.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PROMPT="What can this pokemon do in the Pokemon Trading Card game? Explain in detail."
+MAX_NEW_TOKENS=1024
+
+CUDA_VISIBLE_DEVICES=0 python inference_dataset.py \
+    --model-path checkpoints/llava-v1.5-13b-lora \
+    --model-base liuhaotian/llava-v1.5-13b \
+    --test-file playground/pokemon_llava_dataset_test.jsonl \
+    --image-folder playground/pokemon_images \
+    --query "$PROMPT" \
+    --max_new_tokens $MAX_NEW_TOKENS \
+    --output-file finetune.jsonl
+
+CUDA_VISIBLE_DEVICES=1 python inference_dataset.py \
+    --model-path liuhaotian/llava-v1.5-13b \
+    --test-file playground/pokemon_llava_dataset_test.jsonl \
+    --image-folder playground/pokemon_images \
+    --query "$PROMPT" \
+    --max_new_tokens $MAX_NEW_TOKENS \
+    --output-file base.jsonl
+
+wait
